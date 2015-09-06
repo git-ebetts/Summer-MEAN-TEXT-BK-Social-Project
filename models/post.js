490d166f311c4c491c543d97f3350dcaccc7afc1
git-ebetts <ebettsmail@gmail.com>
2015-09-05 23:05:41 -0400

    reset files

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 var db = require("../db")
 var Post = db.model("Post", {
 	username: { type: String, required: true },
-	body: { type: String },
+	body: { type: String, required: true  },
 	date: { type: Date, default: Date.now }
 })
 module.exports = Post
