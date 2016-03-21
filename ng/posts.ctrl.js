f331b00ede686bf0c947882b75ac65d71001e8db
git-ebetts <ebettsmail@gmail.com>
2016-03-20 23:54:45 -0400

    Added Websocket functionality

--- ng/posts.ctrl.js
+++ ng/posts.ctrl.js
@@ -6,11 +6,17 @@ angular.module('app')
 				username: 'dickeyxxx',
 				body: $scope.postBody
 			}).success(function (post) {
-				$scope.posts.unshift(post)
 				$scope.postBody = null
 			})
 			}
 		}
+
+		  $scope.$on('ws:new_post', function (_, post) {
+    $scope.$apply(function () {
+      $scope.posts.unshift(post)
+    })
+  })
+
 
 		PostsSvc.fetch()
 		.success(function (posts) {
